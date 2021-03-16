b99c8eac0435c1093df9c66920b86e15219b7a63
Caio Vieira <contato@caiovieira.com.br>
2021-03-15 21:26:12 -0300

    feat: ensure DbAddTool returns a tool on success

--- src/data/usecases/add-tool/db-add-tool.ts
+++ src/data/usecases/add-tool/db-add-tool.ts
@@ -4,8 +4,8 @@ import { AddToolRepository } from '../../protocols/add-tool-repository'
 export class DbAddTool implements AddTool {
   constructor (private readonly addToolRepository: AddToolRepository) {}
   async add (data: AddToolModel): Promise<ToolModel> {
-    await this.addToolRepository.add(data)
+    const tool = await this.addToolRepository.add(data)
 
-    return null
+    return tool
   }
 }
